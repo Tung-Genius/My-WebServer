c82945312d26f02bf13fea80d38501a3eab01cd6
Tung <tung@.gmail.com>
2022-11-17 22:00:38 +0700

    setting project

---
+++ src/io/FileIOManager.java
@@ -0,0 +1,20 @@
+package io;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileIOManager {
+	
+	public static String readFileToCharString( String path , Charset encoding )
+			throws IOException {
+		byte[] encoded = Files.readAllBytes( Paths.get( path ) );
+		return new String( encoded , encoding );
+	}
+	
+	public  static byte[] readFileBytes(String path) throws IOException {
+		return Files.readAllBytes( Paths.get( path ) );
+	}
+	
+}
